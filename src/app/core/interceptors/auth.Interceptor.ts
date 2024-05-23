0b0f1e1e405680503b036d2fad27c3e35fe54e23
joe <>
2024-05-24 01:33:38 +0800

    修正 auth interceptor

--- src/app/core/interceptors/auth.Interceptor.ts
+++ src/app/core/interceptors/auth.Interceptor.ts
@@ -4,12 +4,20 @@ import { UserService } from "../../services/user.service";
 import { Observable } from "rxjs";
 import { inject } from "@angular/core";
 
+const skipUrl = ['/api/users/login', '/api/users/register'];
+
 export const authInterceptor: HttpInterceptorFn = (
     req: HttpRequest<any>,
     next: HttpHandlerFn
 ): Observable<HttpEvent<any>> => {
     const userService = inject(UserService);
     const token = userService.getToken();
+    
+    const skip = skipUrl.some(url => req.url.includes(url));
+    if (skip) {
+        return next(req);
+    }
+    
     if (token) {
         const cloned = req.clone({
             setHeaders: {
