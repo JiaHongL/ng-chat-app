b9b9416864ec2f85aec0f5a957ea4b6bb8dcc310
joe <>
2024-05-25 00:33:13 +0800

    修正路徑

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
     apiUrl: 'https://chat-app-backend-t7ug.onrender.com/api',
-    websocketUrl: 'wss://chat-app-backend-t7ug.onrender.com/api',
+    websocketUrl: 'wss://chat-app-backend-t7ug.onrender.com',
 };
