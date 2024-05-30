b5854765584a862682b57d27f81c697688b80e67
joe <>
2024-05-31 01:26:18 +0800

    增加環境變數，調整 deploy

---
+++ src/environments/environment.gh-pages.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: false,
+    apiUrl: 'https://chat-app-backend-t7ug.onrender.com/api',
+    websocketUrl: 'wss://chat-app-backend-t7ug.onrender.com',
+    profileImageApi: 'https://api.dicebear.com/8.x/pixel-art/svg?seed=J_',
+};
