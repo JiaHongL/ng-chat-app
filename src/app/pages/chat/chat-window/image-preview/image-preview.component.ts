666bee902dfeb692cb7d4f2968aab2cacdd84454
joe <>
2024-05-30 23:40:25 +0800

    調整 image preview height width

--- src/app/pages/chat/chat-window/image-preview/image-preview.component.ts
+++ src/app/pages/chat/chat-window/image-preview/image-preview.component.ts
@@ -10,9 +10,9 @@ import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
   ],
   template: `
     <div class="z-40 fixed inset-0 bg-gray-900 bg-opacity-50 flex items-center justify-center">
-      <div class="bg-white p-8 rounded-lg shadow-lg max-w-[550px] max-h-[550px] sm:max-w-[800px] sm:max-h-[800px]">
+      <div class="bg-white p-8 rounded-lg shadow-lg max-w-[550px] max-h-[550px] sm:max-w-[1200px] sm:max-h-[1200px]">
         <div class="flex justify-between items-center mb-6">
-            <img [src]="data.base64String" class="max-w-[450px] max-h-[450px] sm:max-w-[700px] sm:max-h-[700] mx-auto" />
+            <img class="flex-1" [src]="data.base64String" class="max-w-[450px] max-w-h-[450px] sm:max-w-[1150px] sm:max-h-[1150] mx-auto" />
         </div>
         <div class="flex justify-center">
           <button (click)="close()" class="py-2 px-4 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition duration-200">close</button>
