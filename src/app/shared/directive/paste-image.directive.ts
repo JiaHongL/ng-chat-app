1390b33423122633b7a89ba42adee8b753831031
joe <>
2024-05-31 01:09:12 +0800

    修正為貼上最後一個是圖片的

--- src/app/shared/directive/paste-image.directive.ts
+++ src/app/shared/directive/paste-image.directive.ts
@@ -12,17 +12,17 @@ export class PasteImageDirective {
   @HostListener('paste', ['$event'])
   handlePaste(event: ClipboardEvent) {
     const items = event.clipboardData?.items;
-    if (items) {
-      for (let i = 0; i < items.length; i++) {
-        if (items[i].type.indexOf('image') !== -1) {
-          const blob = items[i].getAsFile() as Blob;
-          const reader = new FileReader();
-          reader.onload = (e: any) => {
-            this.imagePasted.emit(e.target.result);
-          };
-          reader.readAsDataURL(blob);
-          event.preventDefault(); // 防止默認行為
-        }
+    if (items && items.length > 0) {
+      const latestItem = items[items.length - 1];
+      if (latestItem.type.indexOf('image') !== -1) {
+        const blob = latestItem.getAsFile() as Blob;
+        const reader = new FileReader();
+        reader.onload = (e: any) => {
+          const imageData = e.target.result;
+          this.imagePasted.emit(imageData);
+        };
+        reader.readAsDataURL(blob);
+        event.preventDefault(); // 防止默認行為
       }
     }
   }
