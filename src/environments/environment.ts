0fb49fba1570489d1f1234185b52b150884f4a7e
joe <>
2024-05-30 21:10:01 +0800

    modify render url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://chat-app-backend-t7ug.onrender.com/api',
-    websocketUrl: 'wss://chat-app-backend-t7ug.onrender.com',
+    apiUrl: 'https://chat-app-backend-9ma9.onrender.com/api',
+    websocketUrl: 'wss://chat-app-backend-9ma9.onrender.com',
 };
