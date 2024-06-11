75ba866782c08f79fa68ca5fdd4df0eddd0f8848
joe <>
2024-06-12 05:12:59 +0800

    api 路徑改為 demp api

--- src/environments/environment.gh-pages.ts
+++ src/environments/environment.gh-pages.ts
@@ -1,6 +1,6 @@
 export const environment = {
-    production: false,
-    apiUrl: 'https://chat-app-backend-t7ug.onrender.com/api',
-    websocketUrl: 'wss://chat-app-backend-t7ug.onrender.com',
+    production: true,
+    apiUrl: 'https://chat-app-backend-9ma9.onrender.com/api',
+    websocketUrl: 'wss://chat-app-backend-9ma9.onrender.com',
     profileImageApi: 'https://api.dicebear.com/8.x/pixel-art/svg?seed=J_',
 };
